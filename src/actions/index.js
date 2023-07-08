c456ae092b8e57bdd283c229bca83cfe7ad5ab20
Nergis <armaganergis@gmail.com>
2023-07-08 14:25:29 +0300

    bitti

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY= "CLEAR_DİSPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const ADD_MEMORY = "ADD_MEMORY";
+export const MEMORY_RECALL = "MEMORY_RECALL";
+export const MEMORY_RESET = "MEMORY_RESET";
 
 export const addOne = () => {
   return ({ type: ADD_ONE });
@@ -9,4 +12,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
   return ({ type: APPLY_NUMBER, payload: number });
+}
+
+export const changeOperation = (operator) => {
+  return ({ type: CHANGE_OPERATION, payload: operator });
+}
+
+export const clearDisplay = () => {
+  return ({ type: CLEAR_DISPLAY });
+}
+
+export const addMemory = () => {
+  return ({ type: ADD_MEMORY });
+}
+
+export const recallMemory = () => {
+  return ({ type: MEMORY_RECALL });
+}
+
+export const resetMemory = () => {
+  return ({ type: MEMORY_RESET });
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,15 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE,
+        APPLY_NUMBER,
+        CHANGE_OPERATION, 
+        CLEAR_DISPLAY,
+        ADD_MEMORY,
+        MEMORY_RECALL,
+        MEMORY_RESET} from './../actions';
 
 export const initialState = {
-  total: 100,
-  operation: "*",
-  memory: 100
+  total: 0,
+  operation: "+",
+  memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -37,6 +43,30 @@ const reducer = (state, action) => {
       return ({
         ...state,
         operation: action.payload
+      });
+
+    case (CLEAR_DISPLAY):
+      return ({
+        ...state,
+        total: 0
+      });
+
+    case (ADD_MEMORY):
+      return ({
+        ...state,
+         memory: state.total
+      });
+
+    case (MEMORY_RECALL):
+      return ({
+        ...state,
+        total: calculateResult(state.total, state.memory, state.operation)
+      });
+
+    case (MEMORY_RESET):
+      return ({
+        ...state,
+        memory: 0
       });
 
     default:
